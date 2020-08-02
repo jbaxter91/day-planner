416cc0625ac33390e4dbdd177cd85ad759c3c962
John Baxter <jbaxter@a-zdarts.com>
2020-08-01 22:11:15 -0700

    Installed @types/jquery to enable intellisence for jquery inside js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,12 @@
 // Script that goes along with index.html
 // This script is responsible for making the day planner interactable
+// ==================================================================
+
+
+// ----- Consts -------
+    // Change this value to chang the starting time of the planner
+const STARTING_HOUR = 7;
+    // Change this value to chang the stopping time of the planner
+const STOPPING_HOUR = 18;
+// ----- END Const ------
+
